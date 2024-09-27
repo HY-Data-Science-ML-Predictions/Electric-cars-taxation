4c3890b182d7cdf28a8795d32dca291371a0a2b8
Matias <matias@devikivi.fi>
2024-09-27 15:51:20 +0300

    Created a function that saves json file data_files folder to make some analyze about the structure of the data; Created data frame from the data

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import requests
 import numpy as np
+import json
 
 def download_and_save_file(url, target_path):
     try:
@@ -15,3 +16,18 @@ def create_year_list(start_year: int, end_year: int) -> np.ndarray[str]|None:
     if start_year and end_year:
         return np.arange(start_year, end_year+1).astype(str)
     return None
+
+
+import json
+
+def json_to_text(json_file, text_output_path):
+    try:
+        pretty_json_str = json.dumps(json_file, indent=4)
+
+        with open(text_output_path, 'w', encoding='utf-8') as text_file:
+            text_file.write(pretty_json_str)
+
+        print(f"JSON data has been successfully written to {text_output_path}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
